4b948a47de16afeda07e93a6ed4e5d783cf87c9a
Berat <yunus-berat1@hotmail.com>
2022-03-15 12:32:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Professor p1;
+
+        p1 = new Professor(" Ensar Gül ");
+        p1.teaches();
+
+        Student s1;
+
+        s1 = new Student(" Berat Özçelebi ");
+        s1.learns();
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student{
+    private String std_name;
+
+    public Student(String name){
+        std_name=name;
+
+    }
+    public void learns(){
+
+        System.out.println("student name"+std_name+"is learning now");
+
+    }
+}
